1257ba0027bc9e4c6114c9503291d4aab434fd86
Agon Bilali <ab29375@seeu.edu.mk>
2024-05-19 23:31:14 +0200

    Unit Testing

---
+++ tests/main.test.js
@@ -0,0 +1,49 @@
+
+/**
+ * @jest-environment jsdom
+ */
+
+test('use jsdom in this test file', () => {
+  const element = document.createElement('div');
+  expect(element).not.toBeNull();
+});
+
+
+const {
+    getRandomWord,
+    updateScore,
+    updateTime,
+    gameOver
+  } = require('../script.js');
+  
+  describe('getRandomWord', () => {
+    test('returns a random word from the words array', () => {
+      const words = ['sigh', 'tense', 'airplane', 'ball'];
+      const randomWord = getRandomWord(words);
+      expect(words).toContain(randomWord);
+    });
+  });
+  
+  describe('updateScore', () => {
+    test('increments the score', () => {
+      let score = 0;
+      updateScore(score);
+      expect(score).toBe(1);
+    });
+  });
+  
+  describe('updateTime', () => {
+    test('decrements the time', () => {
+      let time = 10;
+      updateTime(time);
+      expect(time).toBe(9);
+    });
+  });
+  
+  describe('gameOver', () => {
+    test('displays the game over message', () => {
+      const endgameMessage = gameOver();
+      expect(endgameMessage).toContain('Time ran out');
+    });
+  });
+  
